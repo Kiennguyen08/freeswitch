a4bca53ad86c7debf2ad48b5cba2bf6d8dc4e282
kiennguyen0812 <v.kiennt69@vinbigdata.org>
2023-11-05 22:29:58 +0700

    add escape line at the end of tcp message

--- src/mod/applications/mod_event_websocket/mod_event_websocket.c
+++ src/mod/applications/mod_event_websocket/mod_event_websocket.c
@@ -84,6 +84,7 @@ void send_command(int socket, const char *command)
 {
 	fprintf(stderr, "Command is sending to socket %s\n", command);
 	// Send the command to the event socket
+	strcat(command, "\n");
 	write(socket, command, strlen(command));
 }
 
