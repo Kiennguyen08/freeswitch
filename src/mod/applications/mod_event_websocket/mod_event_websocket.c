e54ed85fbd7745e8cd0f625a1a55601f49c1023c
kiennguyen0812 <v.kiennt69@vinbigdata.org>
2023-11-05 14:33:49 +0700

    using stderr to print log

--- src/mod/applications/mod_event_websocket/mod_event_websocket.c
+++ src/mod/applications/mod_event_websocket/mod_event_websocket.c
@@ -153,14 +153,14 @@ static int callback_websocket(struct lws *wsi, enum lws_callback_reasons reason,
 		// Extract the client ID from the connection request (if available)
 		lws_hdr_copy(wsi, client_id, sizeof(client_id), WSI_TOKEN_GET_URI);
 		add_client(wsi, client_id);
-		printf("WebSocket connection established with client_id %s\n", client_id);
+		fprintf(stderr, "WebSocket connection established with client_id %s\n", client_id);
 		break;
 	case LWS_CALLBACK_RECEIVE:
 		// Handle incoming WebSocket messages
 		// Forward messages to a specific client using the client ID
 		// For example: forward_to_client("client123", (const char *)in);
 		perror("Received websocket message");
-		fprintf(stdout, "Bytes received from Websocket message %.*s\n", (int)len, (char *)in);
+		fprintf(stderr, "Bytes received from Websocket message %.*s\n", (int)len, (char *)in);
 		memcpy(&received_payload.data[LWS_SEND_BUFFER_PRE_PADDING], in, len);
 		received_payload.len = len;
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "Len Receive Data [%lu]", len);
@@ -231,7 +231,7 @@ static void *event_socket_thread(void *arg)
 		// fprintf(stdout, "Bytes received from TCP Payload %d\n", bytes_received);
 		if (bytes_received > 0) {
 			// perror("Event socket receive_data() failed");
-			fprintf(stdout, "Bytes received from TCP Payload %d\n", bytes_received);
+			fprintf(stderr, "Bytes received from TCP Payload %d\n", bytes_received);
 			// Process and forward the received data to WebSocket clients
 			// Here, you should send the data over WebSocket to the connected clients
 			forward_to_all_clients(buffer);
